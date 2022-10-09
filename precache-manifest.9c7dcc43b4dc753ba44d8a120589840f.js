486a6b6d490e2253092862d81bd850653a4cf314
Chris <chrisrawat@email.com>
2022-10-10 01:54:39 +0530

    Updates

---
+++ precache-manifest.9c7dcc43b4dc753ba44d8a120589840f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4df38b02b458fefb96e",
+    "url": "/website/static/js/main.f4df38b0.chunk.js"
+  },
+  {
+    "revision": "f3ba07ae8adf9c0824d2",
+    "url": "/website/static/js/1.f3ba07ae.chunk.js"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "da4cc42a79a67de903903241c74e2122",
+    "url": "/website/index.html"
+  }
+];
